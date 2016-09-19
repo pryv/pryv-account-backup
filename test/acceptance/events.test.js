c77bf5e30f4781222bf7f9f796e35ec694cfe6fc
ikebets <ilia.kebets@epfl.ch>
2016-09-19 14:49:05 +0200

    initial tests

---
+++ test/acceptance/events.test.js
@@ -0,0 +1,75 @@
+/*global describe, it, before, after */
+
+var pryv = require('pryv'),
+    async = require('async'),
+    inspect = require('util').inspect;
+
+describe('Events', function () {
+
+  var connection = null,
+      stream = 'testStream1';
+
+  before(function (done) {
+
+    connection = new pryv.Connection(require('../helpers/testuser').credentials);
+
+    connection.batchCall([
+      {
+        method: 'streams.create',
+        params: {
+          id: stream,
+          name: stream
+        }
+      },
+      {
+        method: 'events.create',
+        params: {
+          streamId: stream,
+          type: 'note/txt',
+          content: 'hi, i am a text event'
+        }
+      }
+    ], function (err, res) {
+      if (err) {
+        return done(err);
+      }
+      console.log(res);
+      done();
+    });
+
+  });
+
+  after(function (done) {
+
+    connection.batchCall([
+      {
+        method: 'streams.delete',
+        params: {
+          id: stream,
+        }
+      },
+      {
+        method: 'streams.delete',
+        params: {
+          id: stream,
+          mergeEventsWithParent: false
+        }
+      }
+    ], function (err, res) {
+      if (err) {
+        return done(err);
+      }
+      console.log(inspect(res, false, null));
+      done();
+    })
+  });
+
+  it('should backup events in the right folder', function (done) {
+
+
+
+
+    done();
+  })
+
+});
---
+++ test/helpers/testuser.js
@@ -0,0 +1,7 @@
+module.exports = {
+  credentials: {
+    username: 'app-node-bkp-test',
+    domain: 'pryv.me',
+    auth: 'cit9zsixhcj2135yq7epmn5x5'
+  }
+};
