d21ebaedeec6de45fa21fd228e18ba22f1756e2b
Thiebaud <tmodoux@pryv.com>
2016-09-21 16:41:16 +0200

    Complete api-resources.test

--- test/unit/api-resources.test.js
+++ test/unit/api-resources.test.js
@@ -26,11 +26,40 @@ describe('api-resources', function () {
 
     it('should retrieve the requested Pryv resource and save it to JSON', function (done) {
 
+        var eventsRequest = 'events?fromTime=-2350373077&toTime=' + new Date() / 1000;
+        var streamsRequest = 'streams';
+        if (params.includeTrashed) {
+            eventsRequest += '&state=all';
+            streamsRequest += '?state=all';
+        }
+
         params.resource = 'streams';
 
         api.toJSONFile(params, function(err) {
             should.not.exist(err);
-            should.equal(fs.existsSync(backupDir+'streams.json'),true);
+            should.equal(fs.existsSync(backupDir+params.resource+'.json'),true);
+            done();
+        });
+    });
+
+    it('should retrieve the requested augmented Pryv resource and save it to JSON', function (done) {
+
+        params.resource = 'events?fromTime=-2350373077&toTime=' + new Date() / 1000 + '&state=all';
+
+        api.toJSONFile(params, function(err) {
+            should.not.exist(err);
+            should.equal(fs.existsSync(backupDir+'events.json'),true);
+            done();
+        });
+    });
+
+    it('should not retrieve and save to JSON invalid requested Pryv resource', function (done) {
+
+        params.resource = 'notvalid';
+
+        api.toJSONFile(params, function(err) {
+            should.exist(err);
+            should.equal(fs.existsSync(backupDir+params.resource+'.json'),false);
             done();
         });
     });
