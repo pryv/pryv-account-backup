46f27c3cf872a8d084cfb6eb88019a525571cb03
loic <loic@pryv.com>
2019-11-15 13:55:48 +0100

    fix kickstart backup logic

--- src/kickstart.js
+++ src/kickstart.js
@@ -1,4 +1,23 @@
 const backup = require('./main');
 const config = require('./utils/config.js');
 const params = config.get('params');
-backup.start(params);
+const BackupDirectory = require('./methods/backup-directory');
+let domain;
+try { // service info
+    url = new URL(params.domain);
+    domain = url.hostname;
+}
+catch(error) { // domain
+    domain = params.domain;
+}
+params.backupDirectory = new BackupDirectory(params.username, domain);
+
+async function start() {
+    await backup.start(params, done);
+}
+
+function done() {
+    console.log('The End');
+}
+
+start();
